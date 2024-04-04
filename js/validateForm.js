660cb449196b30c80eee163a8699ed4b6b62d83c
Pushkin <73908956+PushkinSharma@users.noreply.github.com>
2024-04-04 17:48:02 +0530

    Many changes, made site live

---
+++ js/validateForm.js
@@ -0,0 +1,4 @@
+document.getElementById('contactBtn').addEventListener('click', function() {
+    event.preventDefault();
+    alert('Thank you for your message! We will get back to you soon.')
+});
